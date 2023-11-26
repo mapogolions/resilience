9872a446497b078b5ab4850ab9101402f81fe26e
mapogolions <ivanovpavelalex45@gmail.com>
2023-11-27 02:02:35 +0500

    increase test coverage

--- bulkhead/bulkhead_test.go
+++ bulkhead/bulkhead_test.go
@@ -4,38 +4,53 @@ import (
 	"context"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/mapogolions/resilience/internal"
 )
 
 func TestBulkhead(t *testing.T) {
-	t.Run("some", func(t *testing.T) {
-		bulkhead := NewBulkhead[int, int](2, 3)
-		barrier := make(chan struct{})
-		f := func(ctx context.Context, index int) (int, error) {
-			<-barrier
-			return index, nil
+	t.Run("should limit concurrency level", func(t *testing.T) {
+		testCases := []struct {
+			concurrency    int
+			queue          int
+			total          int
+			expectedErrors int
+		}{
+			{concurrency: 1, queue: 4, total: 10, expectedErrors: 5},
+			{concurrency: 4, queue: 1, total: 10, expectedErrors: 5},
+			{concurrency: 1, queue: 4, total: 5, expectedErrors: 0},
+			{concurrency: 4, queue: 1, total: 5, expectedErrors: 0},
 		}
-		allDone := sync.WaitGroup{}
-		allDone.Add(10)
-		runningCounter := internal.NewCounter()
-		errorCounter := internal.NewCounter()
-		for i := 0; i < 10; i++ {
-			go func(i int) {
-				defer allDone.Done()
-				if _, cur := runningCounter.Increment(); cur == 10 {
-					close(barrier)
-				}
-				_, err := bulkhead(context.Background(), f, i)
-				if err != nil {
-					errorCounter.Increment()
-				}
-			}(i)
-		}
-		allDone.Wait()
+		for _, testCase := range testCases {
+			bulkhead := NewBulkhead[int, int](testCase.concurrency, testCase.queue)
+			barrier := make(chan struct{})
+			f := func(ctx context.Context, index int) (int, error) {
+				<-barrier
+				time.Sleep(50 * time.Millisecond)
+				return index, nil
+			}
+			allDone := sync.WaitGroup{}
+			allDone.Add(testCase.total)
+			runningCounter := internal.NewCounter()
+			errorCounter := internal.NewCounter()
+			for i := 0; i < testCase.total; i++ {
+				go func(i int) {
+					defer allDone.Done()
+					if _, cur := runningCounter.Increment(); cur == testCase.total {
+						close(barrier)
+					}
+					_, err := bulkhead(context.Background(), f, i)
+					if err != nil {
+						errorCounter.Increment()
+					}
+				}(i)
+			}
+			allDone.Wait()
 
-		if errorCounter.Value() != 5 {
-			t.Fail()
+			if errorCounter.Value() != testCase.expectedErrors {
+				t.Fail()
+			}
 		}
 	})
 }
