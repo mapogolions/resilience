3bd5210cd5993f52184a48d8f8ba52ccb134ec26
mapogolions <ivanovpavelalex45@gmail.com>
2024-01-20 02:12:03 +0500

    recovery from crucial error

--- policy/fallback.go
+++ policy/fallback.go
@@ -2,6 +2,8 @@ package policy
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/mapogolions/resilience"
 )
@@ -26,4 +28,40 @@ func NewFallbackPolicy[S any, T any](
 		}
 		return result, err
 	}
+}
+
+func NewPanicFallbackPolicy[S any, T any](
+	fallback Fallback[T],
+	resultPredicates resilience.ResultPredicates[T],
+	errorPredicates resilience.ErrorPredicates,
+) resilience.Policy[S, T] {
+	return func(ctx context.Context, f func(context.Context, S) (T, error), s S) (T, error) {
+		var crucialErr error
+		var result T
+		var err error
+		tryCatch(func() {
+			result, err = f(ctx, s)
+		}, &crucialErr)
+		if crucialErr != nil {
+			return result, crucialErr
+		}
+		return result, err
+	}
+}
+
+func tryCatch(f func(), err *error) {
+	defer func() {
+		if info := recover(); info != nil {
+			if errorMessage, ok := info.(string); ok {
+				*err = errors.New(errorMessage)
+				return
+			}
+			if ex, ok := info.(error); ok {
+				*err = ex
+				return
+			}
+			*err = fmt.Errorf("%v", info)
+		}
+	}()
+	f()
 }
