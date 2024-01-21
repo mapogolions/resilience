bb5ebc16cdf6611ca0c736aebdd4485563191f46
mapogolions <ivanovpavelalex45@gmail.com>
2024-01-21 17:23:51 +0500

    remove redundant type parameter

--- policy/retry.go
+++ policy/retry.go
@@ -8,23 +8,23 @@ import (
 )
 
 type RetryPred[T any, R any] func(resilience.PolicyOutcome[T]) (bool, R)
-type OnRetryFunc[S any, T any, R any] func(context.Context, resilience.PolicyOutcome[T], R)
+type OnRetryFunc[T any, R any] func(context.Context, resilience.PolicyOutcome[T], R)
 
 func OnRetryIdentity[S any, T any, R any](context.Context, resilience.PolicyOutcome[T], R) {}
 func OnRetryCountIdentity[S any, T any](context.Context, resilience.PolicyOutcome[T], int) {}
 
-func NewRetryCountPolicy[S any, T any](retryCount int, onRetry OnRetryFunc[S, T, int]) resilience.Policy[S, T] {
+func NewRetryCountPolicy[S any, T any](retryCount int, onRetry OnRetryFunc[T, int]) resilience.Policy[S, T] {
 	var attempts atomic.Int64
 	shouldRetry := func(outcome resilience.PolicyOutcome[T]) (bool, int) {
 		cur := int(attempts.Add(1))
 		return cur >= retryCount, cur
 	}
-	return NewRetryPolicy(shouldRetry, onRetry)
+	return NewRetryPolicy[S, T, int](shouldRetry, onRetry)
 }
 
 func NewRetryPolicy[S any, T any, R any](
 	shouldRetry RetryPred[T, R],
-	onRetry OnRetryFunc[S, T, R],
+	onRetry OnRetryFunc[T, R],
 ) resilience.Policy[S, T] {
 	return func(ctx context.Context, f func(context.Context, S) (T, error), s S) (T, error) {
 		var result T
