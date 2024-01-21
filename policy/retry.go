c3ce2dff358d90dfeea9cf497a346545e996b795
mapogolions <ivanovpavelalex45@gmail.com>
2024-01-21 17:31:49 +0500

    update retry policy

--- policy/retry.go
+++ policy/retry.go
@@ -7,36 +7,34 @@ import (
 	"github.com/mapogolions/resilience"
 )
 
-type RetryPred[T any, R any] func(resilience.PolicyOutcome[T]) (bool, R)
+type RetryPred[T any] func(context.Context, resilience.PolicyOutcome[T]) bool
 type OnRetryFunc[T any, R any] func(context.Context, resilience.PolicyOutcome[T], R)
 
-func OnRetryIdentity[S any, T any, R any](context.Context, resilience.PolicyOutcome[T], R) {}
-func OnRetryCountIdentity[S any, T any](context.Context, resilience.PolicyOutcome[T], int) {}
+func OnRetryCountIdentity[T any](context.Context, resilience.PolicyOutcome[T], int) {}
 
 func NewRetryCountPolicy[S any, T any](retryCount int, onRetry OnRetryFunc[T, int]) resilience.Policy[S, T] {
 	var attempts atomic.Int64
-	shouldRetry := func(outcome resilience.PolicyOutcome[T]) (bool, int) {
+	shouldRetry := func(ctx context.Context, outcome resilience.PolicyOutcome[T]) bool {
 		cur := int(attempts.Add(1))
-		return cur >= retryCount, cur
+		if cur >= retryCount {
+			return false
+		}
+		onRetry(ctx, outcome, cur)
+		return true
 	}
-	return NewRetryPolicy[S, T, int](shouldRetry, onRetry)
+	return NewRetryPolicy[S, T, int](shouldRetry)
 }
 
-func NewRetryPolicy[S any, T any, R any](
-	shouldRetry RetryPred[T, R],
-	onRetry OnRetryFunc[T, R],
-) resilience.Policy[S, T] {
+func NewRetryPolicy[S any, T any, R any](shouldRetry RetryPred[T]) resilience.Policy[S, T] {
 	return func(ctx context.Context, f func(context.Context, S) (T, error), s S) (T, error) {
 		var result T
 		var err error
 		for {
 			result, err = f(ctx, s)
 			outcome := resilience.PolicyOutcome[T]{Result: result, Err: err}
-			if ok, r := shouldRetry(outcome); ok {
-				onRetry(ctx, outcome, r)
-				continue
+			if !shouldRetry(ctx, outcome) {
+				return result, err
 			}
-			return result, err
 		}
 	}
 }
