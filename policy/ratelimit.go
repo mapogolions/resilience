b017398d744f668c0673987b27c8badfee300cb3
mapogolions <ivanovpavelalex45@gmail.com>
2024-01-23 15:21:39 +0500

    update signature of rate limit condition

--- policy/ratelimit.go
+++ policy/ratelimit.go
@@ -11,14 +11,11 @@ import (
 
 var ErrRateLimitRejected = errors.New("rate limit rejected")
 
-type RateLimitCondition func() bool
+type RateLimitCondition func() (bool, time.Duration)
 
 func NewTokenBucketRateLimitCondition(tokenPerUnit time.Duration, capacity int64) RateLimitCondition {
 	rateLimiter := internal.NewLockFreeRateLimiter(tokenPerUnit, capacity, internal.DefaultTimeProvider)
-	return func() bool {
-		ok, _ := rateLimiter.Try()
-		return !ok
-	}
+	return rateLimiter.Try
 }
 
 func NewTokenBucketRateLimitPolicy[S any, T any](tokenPerUnit time.Duration, capacity int64) resilience.Policy[S, T] {
@@ -29,7 +26,7 @@ func NewTokenBucketRateLimitPolicy[S any, T any](tokenPerUnit time.Duration, cap
 func NewRateLimitPolicy[S any, T any](shouldLimit RateLimitCondition) resilience.Policy[S, T] {
 	var defaultT T
 	return func(ctx context.Context, f func(context.Context, S) (T, error), s S) (T, error) {
-		if shouldLimit() {
+		if ok, _ := shouldLimit(); !ok {
 			return defaultT, ErrRateLimitRejected
 		}
 		return f(ctx, s)
