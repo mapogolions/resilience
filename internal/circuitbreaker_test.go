13fa13fc7ad50396128c7a0babb14ba19f8f0270
mapogolions <ivanovpavelalex45@gmail.com>
2024-01-31 02:13:30 +0500

    inrease test coverage

--- internal/circuitbreaker_test.go
+++ internal/circuitbreaker_test.go
@@ -7,7 +7,47 @@ import (
 )
 
 func TestCircuitBreaker(t *testing.T) {
-	t.Run("circuit should be half open after specified break period", func(t *testing.T) {
+	t.Run("should close circuit whe it is half open and next call succeeded", func(t *testing.T) {
+		// Arrange
+		breakAfter := 2
+		breakDuration := 2 * time.Second
+		timeProvider := NewFakeTimeProvider()
+		cb := NewCircuitBreaker[int](breakAfter, breakDuration, timeProvider)
+
+		// Act
+		cb.Failure(-1, errors.New("err1"))
+		cb.Failure(-2, errors.New("err2"))
+		timeProvider.Advance(breakDuration)
+		cb.IsCircuitOpen() // half open state
+		cb.Success()
+
+		// Assert
+		if cb.IsCircuitOpen() || cb.consecutiveFailures != 0 || cb.state != circuitStateClosed {
+			t.Fail()
+		}
+	})
+
+	t.Run("should open circuit when it is half open and failure happens", func(t *testing.T) {
+		// Arrange
+		breakAfter := 2
+		breakDuration := 2 * time.Second
+		timeProvider := NewFakeTimeProvider()
+		cb := NewCircuitBreaker[int](breakAfter, breakDuration, timeProvider)
+
+		// Act
+		cb.Failure(-1, errors.New("err1"))
+		cb.Failure(-2, errors.New("err2"))
+		timeProvider.Advance(breakDuration)
+		cb.IsCircuitOpen() // half open state
+		cb.Failure(-3, errors.New("err3"))
+
+		// Assert
+		if !cb.IsCircuitOpen() {
+			t.Fail()
+		}
+	})
+
+	t.Run("should half open circuit after specified break period", func(t *testing.T) {
 		breakAfter := 1
 		breakDuration := 2 * time.Second
 		timeProvider := NewFakeTimeProvider()
