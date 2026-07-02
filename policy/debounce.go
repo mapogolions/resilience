b8636df27f9c2ed09b201c7f912c13ec61ada34e
mapogolions <ivanovpavelalex45@gmail.com>
2026-07-02 16:35:37 +0500

    remove debounce last, fix blocking concurrent calls on slowF

--- policy/debounce.go
+++ policy/debounce.go
@@ -15,15 +15,11 @@ type DebouncePolicyKind int
 
 const (
 	DebounceFirst DebouncePolicyKind = 0
-	DebounceLast  DebouncePolicyKind = 1
 )
 
 func NewDebouncePolicy[S any, T any](d time.Duration, kind DebouncePolicyKind) resilience.Policy[S, T] {
 	if kind == DebounceFirst {
 		return debounceFirst[S, T](d)
-	}
-	if kind == DebounceLast {
-		return debounceLast[S, T](d)
 	}
 	panic("not supported")
 }
@@ -34,56 +30,17 @@ func debounceFirst[S any, T any](d time.Duration) resilience.Policy[S, T] {
 
 	return func(ctx context.Context, f func(context.Context, S) (T, error), s S) (T, error) {
 		m.Lock()
-		defer func() {
-			lastCallTime = time.Now().Add(d)
+		now := time.Now()
+
+		if now.Before(lastCallTime) {
 			m.Unlock()
-		}()
-		if time.Now().Before(lastCallTime) {
 			var zero T
 			return zero, ErrDebounced
 		}
+		lastCallTime = now.Add(d)
+		m.Unlock()
+
 		return f(ctx, s)
 	}
-}
 
-func debounceLast[S any, T any](d time.Duration) resilience.Policy[S, T] {
-	var (
-		zero, result T
-		err          error = ErrDebounced
-		timer        *time.Timer
-		lastCall     call[S]
-		m            = sync.Mutex{}
-	)
-
-	return func(ctx context.Context, f func(context.Context, S) (T, error), s S) (T, error) {
-		m.Lock()
-		defer m.Unlock()
-
-		lastCall = call[S]{ctx: ctx, s: s}
-
-		r, e := result, err
-		if timer != nil {
-			timer.Reset(d)
-			return r, e
-		}
-
-		result, err = zero, ErrDebounced
-		timer = time.NewTimer(d)
-
-		// TODO: should be able to cancel last call (ctx.Done)
-		go func() {
-			<-timer.C
-			m.Lock()
-			defer m.Unlock()
-			result, err = f(lastCall.ctx, lastCall.s)
-			timer = nil
-		}()
-
-		return r, e
-	}
-}
-
-type call[S any] struct {
-	ctx context.Context
-	s   S
 }
--- policy/debounce_test.go
+++ policy/debounce_test.go
@@ -7,48 +7,6 @@ import (
 	"time"
 )
 
-func TestDebounceLast(t *testing.T) {
-	t.Run("should return value after specified timeout", func(t *testing.T) {
-		// arrange
-		d := 100 * time.Millisecond
-		timer := time.NewTimer(d)
-		policy := NewDebouncePolicy[int, int](d, DebounceLast)
-		f := newSliceIndexer([]int{10, 20, 30})
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		// act + assert
-		result, err := policy(ctx, f, 1)
-
-		if !errors.Is(err, ErrDebounced) || result != 0 {
-			t.Fail()
-		}
-
-		<-timer.C
-
-		result, err = policy(ctx, f, 1)
-		if err != nil {
-			t.Fail()
-		}
-	})
-
-	t.Run("should return debounced error on the first call", func(t *testing.T) {
-		// arrange
-		policy := NewDebouncePolicy[int, int](50*time.Millisecond, DebounceLast)
-		f := newSliceIndexer([]int{10, 20, 30})
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-
-		// act
-		result, err := policy(ctx, f, 1)
-
-		// assert
-		if !errors.Is(err, ErrDebounced) || result != 0 {
-			t.Fail()
-		}
-	})
-}
-
 func TestDebounceFirst(t *testing.T) {
 	t.Run("should debounce second call within window when context already cancelled", func(t *testing.T) {
 		// arrange
@@ -85,8 +43,8 @@ func TestDebounceFirst(t *testing.T) {
 
 	t.Run("should not suppress calls after debounce window", func(t *testing.T) {
 		// arrange
-		d := 500 * time.Millisecond
-		timer := time.NewTimer(d)
+		d := 200 * time.Millisecond
+		timer := time.NewTimer(d * 2)
 		policy := NewDebouncePolicy[int, int](d, DebounceFirst)
 		f := newSliceIndexer([]int{10, 20, 30})
 		ctx, cancel := context.WithCancel(context.Background())
