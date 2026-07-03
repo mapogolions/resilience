4e4c9ea4c719a898d5499c0d41bec0f0a33a5467
mapogolions <ivanovpavelalex45@gmail.com>
2026-07-04 00:29:27 +0500

    tests for delay

---
+++ policy/delay_test.go
@@ -0,0 +1,30 @@
+package policy
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+)
+
+func TestDelayPolicy(t *testing.T) {
+	t.Run("should be able to cancel delayed call", func(t *testing.T) {
+		t.Parallel()
+
+		// arrange
+		ctx, cancel := context.WithCancel(context.Background())
+		policy := Delay[string, int](24 * time.Hour)
+		strlen := func(ctx context.Context, s string) (int, error) {
+			return len(s), nil
+		}
+
+		// act
+		time.AfterFunc(200*time.Millisecond, cancel)
+		result, err := policy(ctx, strlen, "foo")
+
+		// assert
+		if !errors.Is(err, context.Canceled) || result != 0 {
+			t.Fail()
+		}
+	})
+}
