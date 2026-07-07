96459d90be1731ed013b455874c34d8f0a7df86b
mapogolions <ivanovpavelalex45@gmail.com>
2026-07-07 17:03:28 +0500

    update sample

--- examples/retry/main.go
+++ examples/retry/main.go
@@ -1,31 +1,14 @@
 package main
 
 import (
-	"context"
-	"time"
-
 	"github.com/mapogolions/resilience/policy"
 )
 
 func main() {
 	type S string
 	type T []byte
-	var N int
 
 	policy.NewRetryPolicy[S, T](
-		policy.NewRetryCountOnErrorCondition[T](10),
+		policy.RetryOnError[T](10),
 	)
-
-	policy.NewRetryPolicy[S, T](
-		policy.NewRetryCountOnErrorWithDelayCondition[T](
-			N,
-			func(i int) time.Duration {
-				return time.Duration(i) * time.Second
-			},
-		),
-	)
-
-	policy.NewRetryPolicy[S, T](func(ctx context.Context, outcome policy.Outcome[T], retries int) bool {
-		panic("not implemented")
-	})
 }
