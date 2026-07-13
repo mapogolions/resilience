f511a0052fad323b4bd0259bf6139f9d3e8ee385
mapogolions <ivanovpavelalex45@gmail.com>
2026-07-13 12:09:25 +0500

    leading-edge debounce

--- debounce.go
+++ debounce.go
@@ -14,16 +14,19 @@ func (pf PolicyFunc[S, T]) DebounceFirst(d time.Duration) PolicyFunc[S, T] {
 }
 
 func NewDebounceFirstPolicy[S any, T any](d time.Duration) Policy[S, T] {
-	var nextCallTime time.Time
-	m := sync.Mutex{}
+	var (
+		zero         T
+		nextCallTime time.Time
+		m            = sync.Mutex{}
+	)
 
 	return func(ctx context.Context, f func(context.Context, S) (T, error), s S) (T, error) {
 		m.Lock()
 		now := time.Now()
 
 		if now.Before(nextCallTime) {
+			nextCallTime = now.Add(d)
 			m.Unlock()
-			var zero T
 			return zero, ErrDebounced
 		}
 		nextCallTime = now.Add(d)
